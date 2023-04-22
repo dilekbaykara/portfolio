c134d582a2f3288f71482be60445bd535ca7b0aa
Dilek Baykara <baykara.d@gmail.com>
2023-04-21 23:37:45 -0400

    styling and adding elements for portfolio

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Dilek Baykara</h1>
+        <div id="menu-bar">
+          <h3>About</h3>
+          <h3>Projects</h3>
+          <h3>Resume</h3>
+          <h3>Dark</h3>
+        </div>
       </header>
+      <hr></hr>
+
+      <div id="about"></div>
+      <div id="about-me">About Me</div>
+      <p>
+        I'm an ambitious, self-taught developer who loves working through
+        problems.
+      </p>
+      <div id="skills">Current Skills</div>
+      <p>Javascript</p>
+      <p>React</p>
+      <p>TypeScript</p>
     </div>
   );
 }
