5cb2e19692da34ba8cddf91d7c595e0ae15c0d4e
Dilek Baykara <baykara.d@gmail.com>
2023-05-30 22:06:09 -0400

    working on styling page, Added static header bar and scrollable content

--- src/App.tsx
+++ src/App.tsx
@@ -3,27 +3,56 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Dilek Baykara</h1>
-        <div id="menu-bar">
-          <h3>About</h3>
-          <h3>Projects</h3>
-          <h3>Resume</h3>
-          <h3>Dark</h3>
+      <div className="header-container">
+        <header id="App-header">
+          <h1>Dilek Baykara</h1>
+          <div id="menu-bar">
+            <h3>About</h3>
+            <h3>Projects</h3>
+            <h3>Resume</h3>
+            <input type="checkbox" />
+            <h3>Dark</h3>
+          </div>
+        </header>
+        <hr />
+      </div>
+      <div className="content">
+        <div id="intro">Hi, My name is Dilek Baykara</div>
+        <div id="about">
+          <div id="about-me">About Me</div>
+          <p>
+            I'm an ambitious, self-taught developer who loves working through
+            problems.
+          </p>
         </div>
-      </header>
-      <hr></hr>
+        <div id="skills-box">
+          <div id="skills">Current Skills</div>
+          <p>Javascript</p>
+          <p>React</p>
+          <p>TypeScript</p>
+          <p>Git</p>
+          <p>CSS</p>
+          <p>BootStrap</p>
+          <p>HTML</p>
+          <p>Photoshop</p>
+          <p>Illustrator</p>
+        </div>
+        <div id="projects-div">
+          <div id="projects">Projects</div>
 
-      <div id="about"></div>
-      <div id="about-me">About Me</div>
-      <p>
-        I'm an ambitious, self-taught developer who loves working through
-        problems.
-      </p>
-      <div id="skills">Current Skills</div>
-      <p>Javascript</p>
-      <p>React</p>
-      <p>TypeScript</p>
+          <div>Weather App</div>
+          <div>To do List</div>
+          <div>Restaurant</div>
+          <div>Tic Tac Toe Game</div>
+          <div>Library</div>
+          <div>Rock Paper Scissors</div>
+          <div></div>
+        </div>
+        <div id="resume">
+          Let's work together!
+          <button id="resume-button">Resume</button>
+        </div>
+      </div>
     </div>
   );
 }
