445d7703bbb8460ccca8ef0d5677e9a942e04e56
Dilek Baykara <baykara.d@gmail.com>
2023-05-30 22:33:06 -0400

    adding light and dark mode to portfolio website

--- src/App.tsx
+++ src/App.tsx
@@ -1,56 +1,70 @@
+import { useState } from "react";
 import "./App.css";
 
+// eslint-disable-next-line react-hooks/rules-of-hooks
+
 function App() {
+  const [darkMode, setDarkMode] = useState(false);
+  const toggleDarkMode = () => {
+    setDarkMode(!darkMode);
+  };
   return (
     <div className="App">
-      <div className="header-container">
-        <header id="App-header">
-          <h1>Dilek Baykara</h1>
-          <div id="menu-bar">
-            <h3>About</h3>
-            <h3>Projects</h3>
-            <h3>Resume</h3>
-            <input type="checkbox" />
-            <h3>Dark</h3>
-          </div>
-        </header>
-        <hr />
-      </div>
-      <div className="content">
-        <div id="intro">Hi, My name is Dilek Baykara</div>
-        <div id="about">
-          <div id="about-me">About Me</div>
-          <p>
-            I'm an ambitious, self-taught developer who loves working through
-            problems.
-          </p>
-        </div>
-        <div id="skills-box">
-          <div id="skills">Current Skills</div>
-          <p>Javascript</p>
-          <p>React</p>
-          <p>TypeScript</p>
-          <p>Git</p>
-          <p>CSS</p>
-          <p>BootStrap</p>
-          <p>HTML</p>
-          <p>Photoshop</p>
-          <p>Illustrator</p>
-        </div>
-        <div id="projects-div">
-          <div id="projects">Projects</div>
+      <div className={darkMode ? "dark" : "light"}>
+        <div className="header-container">
+          <header id="App-header">
+            <h1>Dilek Baykara</h1>
+            <div id="menu-bar">
+              <h3>About</h3>
+              <h3>Projects</h3>
+              <h3>Resume</h3>
 
-          <div>Weather App</div>
-          <div>To do List</div>
-          <div>Restaurant</div>
-          <div>Tic Tac Toe Game</div>
-          <div>Library</div>
-          <div>Rock Paper Scissors</div>
-          <div></div>
+              <input type="checkbox" onClick={toggleDarkMode} />
+              <h3>Dark</h3>
+            </div>
+          </header>
+          <hr />
         </div>
-        <div id="resume">
-          Let's work together!
-          <button id="resume-button">Resume</button>
+        <div className="content">
+          <div id="intro">Hi, My name is Dilek Baykara</div>
+          <div id="about">
+            <div id="about-me">About Me</div>
+            <p>
+              I'm an ambitious, self-taught developer who loves working through
+              problems.
+            </p>
+          </div>
+          <div id="skills-box">
+            <div id="skills">Current Skills</div>
+            <p>Javascript</p>
+            <p>React</p>
+            <p>TypeScript</p>
+            <p>Git</p>
+            <p>CSS</p>
+            <p>BootStrap</p>
+            <p>HTML</p>
+            <p>Photoshop</p>
+            <p>Illustrator</p>
+          </div>
+          <div id="projects-div">
+            <div id="projects">Projects</div>
+
+            <div>Weather App</div>
+            <div>To do List</div>
+            <div>Restaurant</div>
+            <div>Tic Tac Toe Game</div>
+            <div>Library</div>
+            <div>Rock Paper Scissors</div>
+            <div></div>
+          </div>
+          <div id="resume-box">
+            <div id="resume">Get in Touch</div>
+            <p>
+              I am looking for new opportunities, if you would like to work
+              together, please reach out!
+            </p>
+            <button id="resume-button">Resume</button>
+          </div>
         </div>
       </div>
     </div>
